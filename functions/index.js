859f1ec50746536965bac68b5e4bf4b5452d83e3
scott-mccartney <smccartney09@gmail.com>
2018-10-07 20:15:34 -0400

    Cloud function now deployable

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
+const {google} = require('googleapis');
+const calendar = google.calendar('v3');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+exports.getListOfCalendars = functions.https.onRequest((request, response) => {
+    // calendar.calendarList.list();
+    // response.send(calendar.calendarList.list());
+    response.send('Hello from getListOfCalendars()');
 });
