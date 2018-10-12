6067a1e70590f863edfd04850f5fa423d1c25583
scott-mccartney <smccartney09@gmail.com>
2018-10-12 16:14:38 -0400

    Add calendar event with successful function response

--- functions/index.js
+++ functions/index.js
@@ -10,44 +10,43 @@ const {google} = require('googleapis');
 const OAuth2 = google.auth.OAuth2; 
 const calendar = google.calendar('v3');
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
-const fs = require('fs'); 
+const googleCredentials = require('./credentials.json');
 
-const GOOGLE_CREDENTIALS_FILE_PATH = '../credentials.json';
-
-admin.initializeApp(functions.config().firebase);
+const ERROR_RESPONSE = {
+    status: "500",
+    message: "There was an error adding an event to your Google calendar"
+};
 
 function addEvent(auth) {
-    const response = calendar.events.insert({
-        auth: auth,
-        calendarId: 'primary',
-        resource: {
-          'summary': 'Event 3',
-          'description': 'Sample description',
-          'start': {
-            'dateTime': '2018-11-11T14:00:00',
-            'timeZone': 'EST',
-          },
-          'end': {
-            'dateTime': '2018-11-11T15:00:00',
-            'timeZone': 'EST',
-          },
-        },
-      }, function(err, res) {
-        if (err) {
-          console.log('Error: ' + err);
-          return;
-        }
-        console.log(res);
-        return res;
-      });
-
-      return response;
+    return new Promise(function(resolve, reject) {
+        calendar.events.insert({
+            auth: auth,
+            calendarId: 'primary',
+            resource: {
+                'summary': 'Test Event',
+                'description': 'Sample description',
+                'start': {
+                    'dateTime': '2018-11-11T14:00:00',
+                    'timeZone': 'EST',
+                },
+                'end': {
+                    'dateTime': '2018-11-11T15:00:00',
+                    'timeZone': 'EST',
+                },
+            },
+        }, (err, res) => {
+            if (err) {
+                console.log('Rejecting because of error');
+                reject(err);
+            }
+            console.log('Request successful');
+            resolve(res.data);
+        });
+    });
 }
 
 function cloudFunction() {
-    const googleCredentials = JSON.parse(fs.readFileSync(GOOGLE_CREDENTIALS_FILE_PATH));
     const oAuth2Client = new OAuth2(
         googleCredentials.web.client_id,
         googleCredentials.web.client_secret,
@@ -57,35 +56,34 @@ function cloudFunction() {
     oAuth2Client.setCredentials({
         refresh_token: googleCredentials.refresh_token
     });
-    addEvent(oAuth2Client);
+    addEvent(oAuth2Client).then(data => {
+        console.log(data);
+        return;
+    }).catch(err => {
+        console.error('Error adding event: ' + err.message);  
+        return;
+    });
 }
 
-cloudFunction();
+// cloudFunction();
 
-// exports.addEventToCalendar = functions.https.onRequest((request, response) => {
-//     admin.storage().bucket().getFiles('credentials.json').then(file => {
-//         const googleCredentials = {};   // Read from Google Cloud Storage bucket
-//         const oAuth2Client = new OAuth2(
-//             googleCredentials.web.client_id,
-//             googleCredentials.web.client_secret,
-//             googleCredentials.web.redirect_uris[0]
-//         );
+exports.addEventToCalendar = functions.https.onRequest((request, response) => {
+    
+    const oAuth2Client = new OAuth2(
+        googleCredentials.web.client_id,
+        googleCredentials.web.client_secret,
+        googleCredentials.web.redirect_uris[0]
+    );
 
-//         oAuth2Client.setCredentials({
-//             refresh_token: googleCredentials.refresh_token
-//         });
-//         addEvent(oAuth2Client).then(res => {
-//             response.status(200).send(res);
-//             return;
-//         }).catch(err => {
-//             response.send(500).send('Error adding event: ' + err.message);
-//             console.log(err);
-//             return;  
-//         });
-//         return;
-//     }).catch(err => {
-//         response.send(500).send('Error reading credentials: ' + err.message);
-//         console.log(err);
-//         return;
-//     });
-// });
+    oAuth2Client.setCredentials({
+        refresh_token: googleCredentials.refresh_token
+    });
+    addEvent(oAuth2Client, response).then(data => {
+        response.status(200).send(data);
+        return;
+    }).catch(err => {
+        console.error('Error adding event: ' + err.message); 
+        response.status(500).send(ERROR_RESPONSE); 
+        return;
+    });
+});
