7f0a1c7b43ce72446f40f7d583ead82ecd40c1ba
scott-mccartney <smccartney09@gmail.com>
2018-10-13 12:04:48 -0400

    Add calendar events based off trigger request body

--- functions/index.js
+++ functions/index.js
@@ -17,22 +17,23 @@ const ERROR_RESPONSE = {
     status: "500",
     message: "There was an error adding an event to your Google calendar"
 };
+const TIME_ZONE = 'EST';
 
-function addEvent(auth) {
+function addEvent(event, auth) {
     return new Promise(function(resolve, reject) {
         calendar.events.insert({
             auth: auth,
             calendarId: 'primary',
             resource: {
-                'summary': 'Test Event',
-                'description': 'Sample description',
+                'summary': event.eventName,
+                'description': event.description,
                 'start': {
-                    'dateTime': '2018-11-11T14:00:00',
-                    'timeZone': 'EST',
+                    'dateTime': event.startTime,
+                    'timeZone': TIME_ZONE,
                 },
                 'end': {
-                    'dateTime': '2018-11-11T15:00:00',
-                    'timeZone': 'EST',
+                    'dateTime': event.endTime,
+                    'timeZone': TIME_ZONE,
                 },
             },
         }, (err, res) => {
@@ -46,29 +47,13 @@ function addEvent(auth) {
     });
 }
 
-function cloudFunction() {
-    const oAuth2Client = new OAuth2(
-        googleCredentials.web.client_id,
-        googleCredentials.web.client_secret,
-        googleCredentials.web.redirect_uris[0]
-    );
-
-    oAuth2Client.setCredentials({
-        refresh_token: googleCredentials.refresh_token
-    });
-    addEvent(oAuth2Client).then(data => {
-        console.log(data);
-        return;
-    }).catch(err => {
-        console.error('Error adding event: ' + err.message);  
-        return;
-    });
-}
-
-// cloudFunction();
-
 exports.addEventToCalendar = functions.https.onRequest((request, response) => {
-    
+    const eventData = {
+        eventName: request.body.eventName,
+        description: request.body.description,
+        startTime: request.body.startTime,
+        endTime: request.body.endTime
+    };
     const oAuth2Client = new OAuth2(
         googleCredentials.web.client_id,
         googleCredentials.web.client_secret,
@@ -78,7 +63,8 @@ exports.addEventToCalendar = functions.https.onRequest((request, response) => {
     oAuth2Client.setCredentials({
         refresh_token: googleCredentials.refresh_token
     });
-    addEvent(oAuth2Client, response).then(data => {
+
+    addEvent(eventData, oAuth2Client).then(data => {
         response.status(200).send(data);
         return;
     }).catch(err => {
